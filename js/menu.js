d6e1287668c0aee1641c0238a4ef2239770a1a59
desipossa <c1jang@hanmail.net>
2024-10-16 11:54:50 +0900

    20241016

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,35 @@
 $(function () {
 
+    $('.mopen').on('click', function () {
+        $('.gnb').toggleClass('on')
+    })
+
     $('.header .gnb>ul>li>a').on('click', function (e) {
-        e.preventDefault();
-        $(this).next().slideToggle();
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+            $(this).next().slideToggle();
+        }
+    });
+
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on');
+        $('.header .gnb>ul>li ul').removeAttr('style')
+    })
+})
+
+$(function () {
+    $('.main_product_slide').slick({
+        arrows: false,
+        slidesToShow: 3,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 1,
+                }
+            }
+        ]
     })
 })
